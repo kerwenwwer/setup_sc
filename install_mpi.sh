17eee3765edcd51698c30252c1cf9278ebe3eb3c
kerwenwwer <kerwen374@gmail.com>
2021-04-30 17:07:41 +0800

    Fix bug in hpl.sh

--- install_mpi.sh
+++ install_mpi.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # use wget to fetch the Intel repository public key
-wget -qO https://apt.repos.intel.com/intel-gpg-keys/GPG-PUB-KEY-INTEL-SW-PRODUCTS.PUB | apt-key add -
+wget -qO- https://apt.repos.intel.com/intel-gpg-keys/GPG-PUB-KEY-INTEL-SW-PRODUCTS.PUB | apt-key add -
 # add to your apt sources keyring so that archives signed with this key will be trusted.
 # remove the public key
 
@@ -13,3 +13,7 @@ add-apt-repository "deb https://apt.repos.intel.com/oneapi all main"
 
 # Install One-api HPC toolkit
 apt update && apt install -y intel-hpckit
+
+# source shell env 
+source /opt/intel/oneapi/setvars.sh
+source /opt/intel/oneapi/modulefiles-setup.sh
